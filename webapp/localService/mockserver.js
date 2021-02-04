7204c11c124b7537636048ef8857964a16a4775b
dungvti <dangdungna@gmail.com>
2021-02-04 16:10:34 +0700

    test

--- webapp/localService/mockserver.js
+++ webapp/localService/mockserver.js
@@ -13,6 +13,7 @@ sap.ui.define(['sap/ui/core/util/MockServer'], function(MockServer) {
          */
 
         init: function() {
+            debugger;
             var oUriParameters = jQuery.sap.getUriParameters(),
                 sJsonFilesUrl = jQuery.sap.getModulePath(_sJsonFilesModulePath),
                 sManifestUrl = jQuery.sap.getModulePath(_sAppModulePath + 'manifest', '.json'),
